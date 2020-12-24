5b7e0c815e00f70065e14a1553802a1cf06c0d68
Andrey Yershov <andreyyersov424@gmail.com>
2020-12-24 10:28:22 +0200

    refactoring code

---
+++ src/main/java/Utilities/ScreenShot.java
@@ -0,0 +1,23 @@
+package Utilities;
+
+import org.openqa.selenium.WebElement;
+import ru.yandex.qatools.ashot.AShot;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class ScreenShot extends CommonOps
+{
+    public static void takeScreenShot(WebElement imageElement, String imageName)
+    {
+        try
+        {
+            imageScreenShot = new AShot().takeScreenshot(driver, imageElement);
+            ImageIO.write(imageScreenShot.getImage(), "png"
+                    , new File(getData("ImageRepository") + imageName + ".png"));
+        } catch (Exception e)
+        {
+            System.out.println("Error copy image file: " + e);
+        }
+    }
+}
